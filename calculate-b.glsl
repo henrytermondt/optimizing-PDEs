656f5e20f30cfa29654b9a53064e1681cbd73c73
henrytermondt <148581276+henrytermondt@users.noreply.github.com>
2025-03-03 11:32:15 -0800

    Renamed variables and added comments

--- calculate-b.glsl
+++ calculate-b.glsl
@@ -7,25 +7,28 @@ uniform sampler2D M;
 uniform vec2 MDim;
 
 void main() {
+    // Texture coordinates for b and x
     vec2 pos = gl_FragCoord.xy - 0.5;
 
+    // Converts b's texture coordinates into M's texture coordinates
     vec2 mPos = vec2(pos.x * float(**c**), pos.y) + 0.5;
-    
+
+    // Multiplies x by M to get the value of b at this position
     vec2 sum = vec2(0, 0);
     for (int i = 0; i < **c**; i ++) {
         vec4 mpix = texture2D(M, (mPos + vec2(i, 0)) / MDim);
         vec2 mval = mpix.xy;
 
-        float vindex = mpix.z; // x coordinate
-        vec2 vpos = vec2(mod(vindex, xDim.x), floor(vindex / xDim.x)) + 0.5;
-        vec2 vval = texture2D(x, vpos / xDim).xy;
+        float xindex = mpix.z; // x coordinate
+        vec2 xpos = vec2(mod(xindex, xDim.x), floor(xindex / xDim.x)) + 0.5;
+        vec2 xval = texture2D(x, xpos / xDim).xy;
 
+        // Adds the complex product to the sum
         sum += vec2(
-            mval.x * vval.x - mval.y * vval.y,
-            mval.x * vval.y + mval.y * vval.x
+            mval.x * xval.x - mval.y * xval.y,
+            mval.x * xval.y + mval.y * xval.x
         );
     }
 
-    vec4 testpx = texture2D(M, vec2(0.5, pos.y + 0.5) / MDim);
-    gl_FragColor = vec4(sum, pos);
+    gl_FragColor = vec4(sum, pos); // Output
 }
