9e87a08f87f63e534a3cd7c06be80c2e37c77f44
henrytermondt <148581276+henrytermondt@users.noreply.github.com>
2025-03-07 09:55:02 -0800

    Cleaned code and added comments

--- setup.js
+++ setup.js
@@ -1,21 +1,25 @@
 // Simulation constants
-const n = 400,//(l / 0.05 | 0) + 1, // Edge length
+const n = 400, // Simulation dimensions
     ni = (n - 2) ** 2, // Size of matricies
     no = (n - 3) * (n - 2); // Offset for lone diagonals
 const dx = 0.05, // Step size 
     dt = dx ** 2 / 4; // Time step size
-const rx = {r: 0, i: dt / (2 * dx ** 2)},
-    ry = {r: 0, i: dt / (2 * dx ** 2)}; 
 const l = n / 20;
 
-const vArr = new Float32Array(ni);
-const imgData = new ImageData(n - 2, n - 2);
+// Constants used to generate A and M
+const rx = {r: 0, i: dt / (2 * dx ** 2)},
+    ry = {r: 0, i: dt / (2 * dx ** 2)}; 
 
+const vArr = new Float32Array(ni); // Data representing the potential barriers
+const imgData = new ImageData(n - 2, n - 2); // Stores visual of the potential barriers
+
+// Formula that dictates where the slits in the potential barrier will be 
 const doubleSlit = x => x ** 4 - 4 * x ** 2 + 3.7;
 
+// Sets the potential barrier and its visual
 const wallWidth = 2;
 for (let i = 0; i < n; i ++) {
-    if (doubleSlit(17.5 * (i - n / 2 + 1) / n) < 0) continue;
+    if (doubleSlit(17.5 * (i - n / 2 + 1) / n) < 0) continue; // If in a slit, skip
     const x = n * 3 / 5 | 0;
 
     for (let w = 0; w < wallWidth; w ++) {
