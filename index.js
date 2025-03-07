bf44092fe2bca1c472603e99716cacab76b47910
henrytermondt <148581276+henrytermondt@users.noreply.github.com>
2025-03-07 09:37:56 -0800

    Added comments

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+// Runs every frame, calculates the next time step
 const loop = () => {
-    calcb();
-    solve();
-    display();
+    calcb(); // Calculates the value of b
+    solve(); // Solves for x
+    display(); // Draws simulation to glCanvas
 
+    // Draws glCanvas and the overlay to canvas so the user can see them
     ctx.drawImage(glCanvas, 0, 0);
     ctx.drawImage(overlay, 0, 0);
 
     window.requestAnimationFrame(loop);
 };
 
+// Loading shaders takes time and is asynchronous, so the simulation must wait to start
 (async () => {
     await loadShaders();
     initShaders();
 
+    // Begin the simulation
     window.requestAnimationFrame(loop);
 })();
