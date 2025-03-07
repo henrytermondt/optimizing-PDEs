f443d4d48fc5be634665b9b209ec4640640b4d39
henrytermondt <148581276+henrytermondt@users.noreply.github.com>
2025-03-07 09:46:13 -0800

    Added comments

--- render.glsl
+++ render.glsl
@@ -6,10 +6,12 @@ uniform vec2 xDim;
 // Gets pixel colors and makes them look a little nicer
 void main() {
     vec3 pix = texture2D(x, gl_FragCoord.xy / xDim).xyz;
+
+    // Gets the squared modulus of the wave function. This is the probability density of measuring a particle at this position
     float module = pix.x * pix.x + pix.y * pix.y;
 
-    vec3 low = vec3(0.02, 0, 0.02);
-    vec3 high = 2.0 * vec3(0.7, 0.5, 0.4);
+    vec3 low = vec3(0.02, 0, 0.02); // Dark color of a gradient
+    vec3 high = 2.0 * vec3(0.7, 0.5, 0.4); // Light color of a gradient
     
     gl_FragColor = vec4(mix(low, high, pow(module, 1.3)), 1);
 }
